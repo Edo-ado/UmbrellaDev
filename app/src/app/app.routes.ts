b47e9caf859f31e0648c5f5b431b1874f5f89b80
Ashkituru <assibajaro@est.utn.ac.cr>
2026-07-09 03:33:43 +0800

    ESPECIALIDAD

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { MainLayout } from './layout/main-layout/main-layout';
 import { Home } from './pages/home/home';
 import { UsuarioLista } from './pages/usuario/usuario-lista/usuario-lista';
 import { CategoriaLista } from './pages/categoria/categoria-lista/categoria-lista';
+import { EspecialidadLista } from './pages/especialidad/lista-especialidad/lista-especialidad';
 
 export const routes: Routes = [
   {
@@ -12,7 +13,7 @@ export const routes: Routes = [
       { path: '', component: Home },
       { path: 'usuarios', component: UsuarioLista },
      { path: 'categorias', component: CategoriaLista },
-
+{ path: 'especialidades', component: EspecialidadLista }
     ],
   },
 ];
