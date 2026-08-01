1c11edb3d9cefd517704b97e756b813d083cee21
Edo-ado <edu18088@gmail.com>
2026-08-01 17:41:00 -0600

    Avance de gestion de permisos

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -5,7 +5,7 @@ import { MainLayout } from './layout/main-layout/main-layout';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainLayout],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
