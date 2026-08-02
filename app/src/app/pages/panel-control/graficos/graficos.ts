6173208417b2ef52d2d43b2959f4679e06806256
Edo-ado <edu18088@gmail.com>
2026-08-01 19:50:23 -0600

    a

---
+++ app/src/app/pages/panel-control/graficos/graficos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graficos',
+  imports: [],
+  templateUrl: './graficos.html',
+  styleUrl: './graficos.css',
+})
+export class Graficos {}
