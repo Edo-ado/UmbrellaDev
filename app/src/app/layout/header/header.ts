58c6351f0b8e45f86d46a762c0fbe46a07685c37
Ashkituru <assibajaro@est.utn.ac.cr>
2026-08-13 20:54:57 -0600

    listo

--- app/src/app/layout/header/header.ts
+++ app/src/app/layout/header/header.ts
@@ -1,18 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrls: ['./header.css'],
 })
-
 export class Header {
+  protected readonly authService = inject(AuthService);
+
   menuOpen = signal(false);
 
   toggleMenu() {
     this.menuOpen.update(value => !value);
+  }
+
+  cerrarSesion(): void {
+    this.authService.logout();
   }
 }
